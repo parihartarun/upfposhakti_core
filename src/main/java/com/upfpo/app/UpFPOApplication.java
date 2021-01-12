c8be6ac19995ecb01b9f5e9dd5e586cb0ea7793c
rakhee123 <rakhee.jain@neosoftmail.com>
2021-01-12 14:23:30 +0530

    Revert "login process"
    This reverts commit 44b6ef9ccf6e2688e290e1614961c847f6879a22.

--- src/main/java/com/upfpo/app/UpFPOApplication.java
+++ src/main/java/com/upfpo/app/UpFPOApplication.java
@@ -2,10 +2,8 @@ package com.upfpo.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.*")
 public class UpFPOApplication {
 
 	public static void main(String[] args) {
