3730e9faa1a73eacf3ef5dcf05fc8c2fc86b10f2
Kaustubh Pise <kaustubh.pise@neosoftmail.com>
2021-01-13 02:45:57 -0800

    Added License Master Entity

---
+++ src/main/java/com/upfpo/app/entity/LicenseMaster.java
@@ -0,0 +1,50 @@
+package com.upfpo.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="license_mst")
+public class LicenseMaster {
+	
+	
+	@Id	
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Integer id;	
+	
+	@Column(name="license_name")
+	private String licenseName;
+	
+	@Column(name="is_active")
+	private boolean isActive;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getLicenseName() {
+		return licenseName;
+	}
+
+	public void setLicenseName(String licenseName) {
+		this.licenseName = licenseName;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+}
