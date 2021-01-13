a74114759da278f4850b221bcdd8152665a7fdab
rakhee123 <rakhee.jain@neosoftmail.com>
2021-01-13 19:25:00 +0530

    my loginrk2 branch merge with develop

---
+++ src/main/java/com/upfpo/app/serviceImpl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.upfpo.app.serviceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.upfpo.app.entity.User;
+import com.upfpo.app.repository.UserRepository;
+import com.upfpo.app.service.UserService;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+}
