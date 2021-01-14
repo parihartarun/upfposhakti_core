41a19ffaa648219859171ec74b76a9ee91576fe4
rakhee123 <rakhee.jain@neosoftmail.com>
2021-01-14 17:49:34 +0530

    role part is done

--- src/main/java/com/upfpo/app/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/upfpo/app/serviceImpl/UserServiceImpl.java
@@ -5,15 +5,28 @@ import org.springframework.stereotype.Service;
 
 import com.upfpo.app.entity.User;
 import com.upfpo.app.repository.UserRepository;
+import com.upfpo.app.repository.UserRoleRepository;
 import com.upfpo.app.service.UserService;
-
-import java.time.LocalDate;
-import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
+	UserRoleRepository userRoleRepository;
+	
+	@Override
+	public User userDetail(String username) {
+		User user = userRepository.findByUserName(username);
+		return user;
+	}
 
-    @Autowired
-    UserRepository userRepository;
+	@Override
+	public String getRoleName(String roleId) {
+		String roleName = userRoleRepository.roleNameById(roleId);
+		return roleName;
+		
+	}
 
 }
--- src/main/java/com/upfpo/app/web_controller/LoginController.java
+++ src/main/java/com/upfpo/app/web_controller/LoginController.java
@@ -18,10 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.upfpo.app.auth.request.LoginRequest;
 import com.upfpo.app.auth.response.JwtResponse;
+import com.upfpo.app.entity.User;
+
 import com.upfpo.app.repository.UserRepository;
 import com.upfpo.app.security.MyUserDetail;
 import com.upfpo.app.security.MyUserDetailService;
 import com.upfpo.app.security.jwt.JwtUtils;
+import com.upfpo.app.service.UserService;
 import com.upfpo.app.util.RandomString;
 
 //@CrossOrigin(origins = "*", maxAge = 3600)
@@ -32,6 +35,9 @@ public class LoginController {
 	@Autowired
 	AuthenticationManager authenticationManager;
 
+	@Autowired
+	UserService userService;
+	
 	@Autowired
 	UserRepository userRepository;
 
@@ -56,8 +62,53 @@ public class LoginController {
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) throws Exception {
 		try {
-		authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+			final UserDetails userDetails = myUserDetailService.loadUserByUsername(loginRequest.getUsername());
+			String jwt = jwtUtils.generateToken(userDetails);
+			
+			if(userDetails.getUsername() != null && userDetails.isEnabled() == true) {
+				User fullUserdetail = userService.userDetail(userDetails.getUsername());
+				if(fullUserdetail != null) {
+					String roleId = fullUserdetail.getRoleRefId();
+					if(roleId != null) {
+						String userRole = userService.getRoleName(roleId);
+						System.err.println("user role == "+userRole);
+						if (userRole.equals("ROLE_MIN")) {
+							//logger.info("ROLE_MIN Logged IN");
+							//return "redirect:/afterLogin/ministry";
+						}
+
+						if (userRole.equals("ROLE_SLA")) {
+							//logger.info("ROLE_SLA Logged IN");
+							//return "redirect:/afterLogin/sla";
+						}
+
+						if (userRole.equals("ROLE_SP")) {
+
+							//logger.info("ROLE_SP Logged IN");
+							//return "redirect:/afterLogin/sp";
+						}
+
+						if (userRole.equals("ROLE_FPC")) {
+
+							//logger.info("ROLE_FPC Logged IN");
+							//return "redirect:/afterLogin/fpc";
+						}
+
+						if (userRole.equals("ROLE_FIG")) {
+
+							//logger.info("ROLE_FIG Logged IN");
+							//return "redirect:/afterLogin/fig";
+						}
+
+						if (userRole.equals("ROLE_FARMER")) {
+							//logger.info("ROLE_FARMER Logged IN");
+							//return "redirect:/afterLogin/farmer";
+						}
+					}
+				}
+			}
 		}
 		catch(BadCredentialsException e){
 			throw new Exception("incorrect credential"+e);
