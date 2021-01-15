53e442a113a2fead7492049ac677acd72e0bd8a7
Nishant Shah <nishant.shah@wwindia.com>
2021-01-15 19:14:08 +0530

    default URL

--- src/main/java/com/upfpo/app/UpFPOApplication.java
+++ src/main/java/com/upfpo/app/UpFPOApplication.java
@@ -3,10 +3,16 @@ package com.upfpo.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class UpFPOApplication {
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UpFPOApplication.class, args);
