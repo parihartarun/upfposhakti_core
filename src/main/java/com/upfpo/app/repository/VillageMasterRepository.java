cdcfc1cb49fc2bdf34e5fd5d8b2cde23223b329e
Rakshita Devkar <rakshita.devkar@neosoftmail.com>
2021-01-18 11:18:10 +0530

    Conflict resolved with signup

--- src/main/java/com/upfpo/app/repository/VillageMasterRepository.java
+++ src/main/java/com/upfpo/app/repository/VillageMasterRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.upfpo.app.entity.VillageMaster;
 
+@Repository
 public interface VillageMasterRepository extends JpaRepository<VillageMaster, Integer>
 {
 	@Query("Select v from VillageMaster v where v.panchayatId= :panchayatId")
