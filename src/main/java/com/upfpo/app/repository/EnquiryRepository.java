cc926c817bf13d0c7a60dfc4b3471b28b38da6c7
rahulpandeneosoft <rahul.pande@neosoftmail.com>
2021-01-19 14:45:59 +0530

    EmailNotification config

---
+++ src/main/java/com/upfpo/app/repository/EnquiryRepository.java
@@ -0,0 +1,9 @@
+package com.upfpo.app.repository;
+
+import com.upfpo.app.entity.Enquiry;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnquiryRepository extends JpaRepository<Enquiry, Long> {
+}
