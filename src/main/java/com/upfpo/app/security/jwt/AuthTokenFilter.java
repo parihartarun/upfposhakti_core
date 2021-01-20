699858f2434ed17ab3499c365a9ba623d12f4ab0
rakhee123 <rakhee.jain@neosoftmail.com>
2021-01-20 15:25:48 +0530

    register farmer

--- src/main/java/com/upfpo/app/security/jwt/AuthTokenFilter.java
+++ src/main/java/com/upfpo/app/security/jwt/AuthTokenFilter.java
@@ -34,14 +34,14 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 			throws ServletException, IOException {
 
 	      System.out.println("WebConfig; "+request.getRequestURI());
-	      response.setHeader("Access-Control-Allow-Origin", "*");
-	      response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-	      response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
-	      response.setHeader("Access-Control-Max-Age", "3600");
-	      response.setHeader("Access-Control-Allow-Credentials", "true");
-	      response.setHeader("Access-Control-Expose-Headers", "Authorization");
-	      response.addHeader("Access-Control-Expose-Headers", "responseType");
-	      response.addHeader("Access-Control-Expose-Headers", "observe");
+//	      response.setHeader("Access-Control-Allow-Origin", "*");
+//	      response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+//	      response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,observe");
+//	      response.setHeader("Access-Control-Max-Age", "3600");
+//	      response.setHeader("Access-Control-Allow-Credentials", "true");
+//	      response.setHeader("Access-Control-Expose-Headers", "Authorization");
+//	      response.addHeader("Access-Control-Expose-Headers", "responseType");
+//	      response.addHeader("Access-Control-Expose-Headers", "observe");
 	      System.out.println("Request Method: "+request.getMethod());
 		
 		final String athorizationHeader = request.getHeader("authorization");
