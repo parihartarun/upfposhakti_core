4f1cd5f0ef2692d709614bd62b7993334a36a803
Prashantsingh2 <prashant3.Singh@in.ey.com>
2021-01-22 14:27:17 +0530

    New bean Addition

--- src/main/java/com/upfpo/app/UpFPOApplication.java
+++ src/main/java/com/upfpo/app/UpFPOApplication.java
@@ -17,7 +17,6 @@ public class UpFPOApplication  {
 		SpringApplication.run(UpFPOApplication.class, args);
 	}
 
-
 	@Bean
 	public JavaMailSender getJavaMailSender() {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
