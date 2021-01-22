04ade3e9c86a2cb36c0f9b61c8c6f10da73da2e2
rahulpandeneosoft <rahul.pande@neosoftmail.com>
2021-01-22 20:57:23 +0530

    revert

--- src/main/java/com/upfpo/app/entity/Notification.java
+++ src/main/java/com/upfpo/app/entity/Notification.java
@@ -1,12 +1,40 @@
 package com.upfpo.app.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Calendar;
 
 @Entity
 @Table(name = "notification")
 public class Notification {
+
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="production_id")
+    private Integer id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "message")
+    private String message;
+
+    @Column(name = "receiver_id")
+    private Integer receiverId;
+
+    @Column(name = "sender_id")
+    private Integer senderId;
+
+    @Column(name ="is_read")
+    private Boolean isRead;
+
+    @Column(name = "notificaton_type")
+    private NotificationType type;
+
+
+
+
 
 
 }
