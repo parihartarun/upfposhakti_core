0a340715e59a48229638baa06ef253ac190836cf
Nishant Shah <nishant.shah@wwindia.com>
2021-01-22 20:32:47 +0530

    merged conflicts

--- src/main/java/com/upfpo/app/repository/CircularsRepository.java
+++ src/main/java/com/upfpo/app/repository/CircularsRepository.java
@@ -1,14 +1,6 @@
 package com.upfpo.app.repository;
 
-<<<<<<< HEAD
-import com.upfpo.app.entity.Circulars;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-
-@Repository
-public interface CircularsRepository extends JpaRepository<Circulars, Integer> {
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -19,5 +11,4 @@ public interface CircularsRepository extends JpaRepository<Circulars, Integer>{
 	
 	
 
->>>>>>> 11eec8db0b7631bb1e9af844d510dacd69cb659e
 }
