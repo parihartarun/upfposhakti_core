b6f7e0e736a9e964fa903151e249a1cc4f80789e
Rakshita Devkar <rakshita.devkar@neosoftmail.com>
2021-01-28 21:20:55 +0530

    Farmer Land Details

--- src/main/java/com/upfpo/app/entity/LandDetails.java
+++ src/main/java/com/upfpo/app/entity/LandDetails.java
@@ -1,4 +1,5 @@
 package com.upfpo.app.entity;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.ColumnResult;
 import javax.persistence.ConstructorResult;
@@ -65,6 +66,14 @@ public class LandDetails {
 	 * @Column(name="farmer_id") private Integer farmerId;
 	 */
 	
+	public FarmerMaster getFarmerProfile() {
+		return farmerProfile;
+	}
+
+	public void setFarmerProfile(FarmerMaster farmerProfile) {
+		this.farmerProfile = farmerProfile;
+	}
+
 	@Column(name="is_organic")
 	private String isorganc;
 	
