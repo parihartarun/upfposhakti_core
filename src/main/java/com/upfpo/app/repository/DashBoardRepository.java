8a1debe0c747f3e042401b7fcc643471349e1f37
Kaustubh Pise <kaustubh.pise@neosoftmail.com>
2021-01-28 18:47:17 +0530

    dashboard code

--- src/main/java/com/upfpo/app/repository/DashBoardRepository.java
+++ src/main/java/com/upfpo/app/repository/DashBoardRepository.java
@@ -1,5 +1,6 @@
 package com.upfpo.app.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.upfpo.app.entity.DashBoardData;
@@ -7,4 +8,8 @@ import com.upfpo.app.entity.DashBoardData;
 @Repository
 public interface DashBoardRepository extends JpaRepository<DashBoardData, Integer> {
 
+	@Query(value="select dbd from DashBoardData dbd")
+	public DashBoardData getFarmCropDetailsForDepatment();
+	
+	
 }
