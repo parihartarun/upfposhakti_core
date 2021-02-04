e733d4896643e3b8a801927920bba1d945272ce2
Rakshita Devkar <rakshita.devkar@neosoftmail.com>
2021-02-04 22:13:00 +0530

    Entity Crop Production

---
+++ src/main/java/com/upfpo/app/repository/CropVarietyRepository.java
@@ -0,0 +1,14 @@
+package com.upfpo.app.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.upfpo.app.entity.CropVerietyMaster;
+
+@Repository
+public interface CropVarietyRepository extends JpaRepository<CropVerietyMaster, Integer>
+{
+	public List<CropVerietyMaster> findByCropRefId(int cropRefId);
+}
