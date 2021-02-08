3cbec04d00ba2cadb4601e230c452de6625969ff
Rakshita Devkar <rakshita.devkar@neosoftmail.com>
2021-02-08 15:20:18 +0530

    Select API of FPO Crop Production

--- src/main/java/com/upfpo/app/service/FPOCropProductionServiceImpl.java
+++ src/main/java/com/upfpo/app/service/FPOCropProductionServiceImpl.java
@@ -15,6 +15,7 @@ import com.upfpo.app.entity.TotalProduction;
 import com.upfpo.app.repository.CropDetailsMasterRepository;
 import com.upfpo.app.repository.FPOCropProductionReporisitory;
 import com.upfpo.app.repository.TotalProductionRepository;
+import com.upfpo.app.util.GetFinYear;
 
 @Service
 public class FPOCropProductionServiceImpl implements FPOCropProductionService {
@@ -41,7 +42,7 @@ public class FPOCropProductionServiceImpl implements FPOCropProductionService {
 					+ "		inner join crop_master cm on cm.id= m.crop_id\r\n"
 					+ "		inner join crop_category cc on cc.id= cm.crop_cat_ref_id \r\n"
 					+ "		left join crop_veriety_master cvm on cvm.veriety_id=cast(m.veriety_id as integer) \r\n"
-					+ "		where m.master_id = :masterId  and m.is_deleted = true order by m.id desc;" ;
+					+ "		where m.master_id = :masterId  and m.is_deleted = false order by m.id desc;" ;
 			  
 			  List<FpoCropProductionDetailsDTO> obj =  (List<FpoCropProductionDetailsDTO>) entityManager.createNativeQuery(sql,"FpoCropProductionDetailsDTO").setParameter("masterId", masterId).getResultList();
 			  return obj;
@@ -59,6 +60,8 @@ public class FPOCropProductionServiceImpl implements FPOCropProductionService {
 		totProd.setFpoRegister(marketableSurplus.getMasterId());
 		totProd.setTotalMarketable(marketableSurplus.getMarketableQuantity());
 		totProd.setTotal_actual_prod(marketableSurplus.getActualQuantity());
+		
+		marketableSurplus.setFinancialYear(GetFinYear.getCurrentFinYear());
 		fpoCropProductionRepo.save(marketableSurplus);
 		totalProductionRepository.save(totProd);
 		
