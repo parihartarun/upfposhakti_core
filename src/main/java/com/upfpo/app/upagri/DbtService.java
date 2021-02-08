df6588b13b53be96dbb0266c39cee66b41f6fd30
Prashantsingh2 <prashant3.Singh@in.ey.com>
2021-02-08 18:00:22 +0530

    UP Agri Pardarshi Integration

---
+++ src/main/java/com/upfpo/app/upagri/DbtService.java
@@ -0,0 +1,16 @@
+/**
+ * DbtService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package com.upfpo.app.upagri;
+
+public interface DbtService extends javax.xml.rpc.Service {
+    public java.lang.String getDbtServiceSoapAddress();
+
+    public com.upfpo.app.upagri.DbtServiceSoap getDbtServiceSoap() throws javax.xml.rpc.ServiceException;
+
+    public com.upfpo.app.upagri.DbtServiceSoap getDbtServiceSoap(java.net.URL portAddress) throws javax.xml.rpc.ServiceException;
+}
