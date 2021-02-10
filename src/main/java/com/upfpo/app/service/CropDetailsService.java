01f6708b17cde4b16ad1395c68a36a74f989e88c
rakhee123 <rakhee.jain@neosoftmail.com>
2021-02-10 16:10:07 +0530

    deparment reports part add changes 2

--- src/main/java/com/upfpo/app/service/CropDetailsService.java
+++ src/main/java/com/upfpo/app/service/CropDetailsService.java
@@ -2,9 +2,13 @@ package com.upfpo.app.service;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.upfpo.app.entity.CropMaster;
 
 public interface CropDetailsService 
 {
 	List<CropMaster> getCropDetails();
+
+	List<CropMaster> getCropsBySeasonId(Integer seasonId);
 }
--- src/main/java/com/upfpo/app/service/CropDetailsServiceImpl.java
+++ src/main/java/com/upfpo/app/service/CropDetailsServiceImpl.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.upfpo.app.entity.CropMaster;
+import com.upfpo.app.entity.DistrictMaster;
 import com.upfpo.app.repository.CropDetailsMasterRepository;
 
 @Service
@@ -20,6 +23,13 @@ public class CropDetailsServiceImpl implements CropDetailsService
 	{
 		List<CropMaster> cropList = new ArrayList<CropMaster>();
 		cropDetailsMasterRepository.findAll().forEach(cropList1->cropList.add(cropList1));
+		return cropList;
+	}
+
+	@Override
+	public List<CropMaster> getCropsBySeasonId(Integer seasonId) {
+		List<CropMaster> cropList = new ArrayList<CropMaster>();
+		cropList = cropDetailsMasterRepository.getDistrictsByStateId(seasonId);
 		return cropList;
 	}
 }
