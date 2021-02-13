2bcf7172d3be0ca64a20a06b6b9a49c887a7ffc3
rahulpandeneosoft <rahul.pande@neosoftmail.com>
2021-02-13 13:21:36 +0530

    File Upload check

--- src/main/java/com/upfpo/app/service/CircularsServiceImpl.java
+++ src/main/java/com/upfpo/app/service/CircularsServiceImpl.java
@@ -130,7 +130,7 @@ public class CircularsServiceImpl implements CircularsService {
             //Path path = Paths.get(fileBasePath + fileName);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
             fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                    .path("uploads/Circular/")
+                    .path("/circulars/download/")
                     .path(fileName)
                     .toUriString();
             circularsRepository.findById(id)
