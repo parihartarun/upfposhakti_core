caacbe01aa351f2bd9c06902af637250fcff6d21
Rakshita Devkar <rakshita.devkar@neosoftmail.com>
2021-02-19 17:26:32 +0530

    CORS Issue

--- src/main/java/com/upfpo/app/controller/FPOCropSowingController.java
+++ src/main/java/com/upfpo/app/controller/FPOCropSowingController.java
@@ -3,6 +3,7 @@ package com.upfpo.app.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,8 @@ import com.upfpo.app.requestStrings.ReportRequestString;
 import com.upfpo.app.service.FPOCropSowingService;
 
 import lombok.Delegate;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
 
 @RestController
 @RequestMapping(value = "/fpoCropSowing")
