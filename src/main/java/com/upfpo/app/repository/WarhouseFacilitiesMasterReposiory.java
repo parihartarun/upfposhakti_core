4da9be67c659febd4d31f8a153d9e7b54c13863d
rahulpandeneosoft <rahul.pande@neosoftmail.com>
2021-03-04 16:59:37 +0530

    Facility added

---
+++ src/main/java/com/upfpo/app/repository/WarhouseFacilitiesMasterReposiory.java
@@ -0,0 +1,7 @@
+package com.upfpo.app.repository;
+
+import com.upfpo.app.entity.WarehouseFacilityMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WarhouseFacilitiesMasterReposiory extends JpaRepository<WarehouseFacilityMaster, Integer> {
+}
